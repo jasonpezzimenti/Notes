0bec7925f43b311d49605d5f2f24fc7a0c81b297
Jason Pezzimenti <96077246+jasonpezzimenti@users.noreply.github.com>
2022-10-25 22:13:33 +1100

    Add project files.

---
+++ Notes/Window.Designer.cs
@@ -0,0 +1,355 @@
+﻿namespace Notes
+{
+	partial class Window
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+			this.mainMenu1 = new System.Windows.Forms.MainMenu(this.components);
+			this.menuItem1 = new System.Windows.Forms.MenuItem();
+			this.menuItem2 = new System.Windows.Forms.MenuItem();
+			this.menuItem3 = new System.Windows.Forms.MenuItem();
+			this.menuItem4 = new System.Windows.Forms.MenuItem();
+			this.menuItem5 = new System.Windows.Forms.MenuItem();
+			this.menuItem6 = new System.Windows.Forms.MenuItem();
+			this.menuItem7 = new System.Windows.Forms.MenuItem();
+			this.menuItem8 = new System.Windows.Forms.MenuItem();
+			this.menuItem9 = new System.Windows.Forms.MenuItem();
+			this.menuItem10 = new System.Windows.Forms.MenuItem();
+			this.menuItem11 = new System.Windows.Forms.MenuItem();
+			this.menuItem12 = new System.Windows.Forms.MenuItem();
+			this.menuItem13 = new System.Windows.Forms.MenuItem();
+			this.menuItem14 = new System.Windows.Forms.MenuItem();
+			this.menuItem15 = new System.Windows.Forms.MenuItem();
+			this.menuItem16 = new System.Windows.Forms.MenuItem();
+			this.menuItem17 = new System.Windows.Forms.MenuItem();
+			this.menuItem18 = new System.Windows.Forms.MenuItem();
+			this.menuItem19 = new System.Windows.Forms.MenuItem();
+			this.menuItem20 = new System.Windows.Forms.MenuItem();
+			this.menuItem21 = new System.Windows.Forms.MenuItem();
+			this.menuItem22 = new System.Windows.Forms.MenuItem();
+			this.menuItem23 = new System.Windows.Forms.MenuItem();
+			this.menuItem24 = new System.Windows.Forms.MenuItem();
+			this.menuItem25 = new System.Windows.Forms.MenuItem();
+			this.menuItem26 = new System.Windows.Forms.MenuItem();
+			this.menuItem27 = new System.Windows.Forms.MenuItem();
+			this.menuItem28 = new System.Windows.Forms.MenuItem();
+			this.menuItem29 = new System.Windows.Forms.MenuItem();
+			this.menuItem30 = new System.Windows.Forms.MenuItem();
+			this.editor = new System.Windows.Forms.TextBox();
+			this.list = new System.Windows.Forms.TreeView();
+			((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+			this.splitContainer1.Panel1.SuspendLayout();
+			this.splitContainer1.Panel2.SuspendLayout();
+			this.splitContainer1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// splitContainer1
+			// 
+			this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.splitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
+			this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+			this.splitContainer1.Name = "splitContainer1";
+			// 
+			// splitContainer1.Panel1
+			// 
+			this.splitContainer1.Panel1.Controls.Add(this.list);
+			// 
+			// splitContainer1.Panel2
+			// 
+			this.splitContainer1.Panel2.Controls.Add(this.editor);
+			this.splitContainer1.Size = new System.Drawing.Size(847, 508);
+			this.splitContainer1.SplitterDistance = 236;
+			this.splitContainer1.TabIndex = 0;
+			// 
+			// mainMenu1
+			// 
+			this.mainMenu1.MenuItems.AddRange(new System.Windows.Forms.MenuItem[] {
+            this.menuItem1,
+            this.menuItem2,
+            this.menuItem3,
+            this.menuItem4,
+            this.menuItem5,
+            this.menuItem6,
+            this.menuItem7});
+			// 
+			// menuItem1
+			// 
+			this.menuItem1.Index = 0;
+			this.menuItem1.MenuItems.AddRange(new System.Windows.Forms.MenuItem[] {
+            this.menuItem8,
+            this.menuItem11,
+            this.menuItem12,
+            this.menuItem13,
+            this.menuItem14,
+            this.menuItem15,
+            this.menuItem16,
+            this.menuItem17,
+            this.menuItem18,
+            this.menuItem19});
+			this.menuItem1.Text = "File";
+			// 
+			// menuItem2
+			// 
+			this.menuItem2.Index = 1;
+			this.menuItem2.MenuItems.AddRange(new System.Windows.Forms.MenuItem[] {
+            this.menuItem20,
+            this.menuItem21,
+            this.menuItem22,
+            this.menuItem23,
+            this.menuItem24,
+            this.menuItem25,
+            this.menuItem26,
+            this.menuItem27,
+            this.menuItem28,
+            this.menuItem29,
+            this.menuItem30});
+			this.menuItem2.Text = "Edit";
+			// 
+			// menuItem3
+			// 
+			this.menuItem3.Index = 2;
+			this.menuItem3.Text = "View";
+			// 
+			// menuItem4
+			// 
+			this.menuItem4.Index = 3;
+			this.menuItem4.Text = "Format";
+			// 
+			// menuItem5
+			// 
+			this.menuItem5.Index = 4;
+			this.menuItem5.Text = "Tools";
+			// 
+			// menuItem6
+			// 
+			this.menuItem6.Index = 5;
+			this.menuItem6.Text = "Window";
+			// 
+			// menuItem7
+			// 
+			this.menuItem7.Index = 6;
+			this.menuItem7.Text = "Help";
+			// 
+			// menuItem8
+			// 
+			this.menuItem8.Index = 0;
+			this.menuItem8.MenuItems.AddRange(new System.Windows.Forms.MenuItem[] {
+            this.menuItem9,
+            this.menuItem10});
+			this.menuItem8.Text = "New";
+			// 
+			// menuItem9
+			// 
+			this.menuItem9.Index = 0;
+			this.menuItem9.Text = "Group";
+			// 
+			// menuItem10
+			// 
+			this.menuItem10.Index = 1;
+			this.menuItem10.Text = "Note";
+			// 
+			// menuItem11
+			// 
+			this.menuItem11.Index = 1;
+			this.menuItem11.Text = "Import...";
+			// 
+			// menuItem12
+			// 
+			this.menuItem12.Index = 2;
+			this.menuItem12.Text = "-";
+			// 
+			// menuItem13
+			// 
+			this.menuItem13.Index = 3;
+			this.menuItem13.Text = "Print Preview";
+			// 
+			// menuItem14
+			// 
+			this.menuItem14.Index = 4;
+			this.menuItem14.Text = "Print";
+			// 
+			// menuItem15
+			// 
+			this.menuItem15.Index = 5;
+			this.menuItem15.Text = "Page Setup";
+			// 
+			// menuItem16
+			// 
+			this.menuItem16.Index = 6;
+			this.menuItem16.Text = "-";
+			// 
+			// menuItem17
+			// 
+			this.menuItem17.Index = 7;
+			this.menuItem17.Text = "Create Backup";
+			// 
+			// menuItem18
+			// 
+			this.menuItem18.Index = 8;
+			this.menuItem18.Text = "-";
+			// 
+			// menuItem19
+			// 
+			this.menuItem19.Index = 9;
+			this.menuItem19.Text = "Exit";
+			// 
+			// menuItem20
+			// 
+			this.menuItem20.Index = 0;
+			this.menuItem20.Text = "Undo";
+			// 
+			// menuItem21
+			// 
+			this.menuItem21.Index = 1;
+			this.menuItem21.Text = "Redo";
+			// 
+			// menuItem22
+			// 
+			this.menuItem22.Index = 2;
+			this.menuItem22.Text = "-";
+			// 
+			// menuItem23
+			// 
+			this.menuItem23.Index = 3;
+			this.menuItem23.Text = "Cut";
+			// 
+			// menuItem24
+			// 
+			this.menuItem24.Index = 4;
+			this.menuItem24.Text = "Copy";
+			// 
+			// menuItem25
+			// 
+			this.menuItem25.Index = 5;
+			this.menuItem25.Text = "Paste";
+			// 
+			// menuItem26
+			// 
+			this.menuItem26.Index = 6;
+			this.menuItem26.Text = "-";
+			// 
+			// menuItem27
+			// 
+			this.menuItem27.Index = 7;
+			this.menuItem27.Text = "Select All";
+			// 
+			// menuItem28
+			// 
+			this.menuItem28.Index = 8;
+			this.menuItem28.Text = "Delete";
+			// 
+			// menuItem29
+			// 
+			this.menuItem29.Index = 9;
+			this.menuItem29.Text = "-";
+			// 
+			// menuItem30
+			// 
+			this.menuItem30.Index = 10;
+			this.menuItem30.Text = "Date/Time";
+			// 
+			// editor
+			// 
+			this.editor.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(250)))), ((int)(((byte)(250)))));
+			this.editor.BorderStyle = System.Windows.Forms.BorderStyle.None;
+			this.editor.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.editor.Location = new System.Drawing.Point(0, 0);
+			this.editor.Multiline = true;
+			this.editor.Name = "editor";
+			this.editor.Size = new System.Drawing.Size(607, 508);
+			this.editor.TabIndex = 0;
+			// 
+			// list
+			// 
+			this.list.BackColor = System.Drawing.Color.WhiteSmoke;
+			this.list.BorderStyle = System.Windows.Forms.BorderStyle.None;
+			this.list.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.list.Location = new System.Drawing.Point(0, 0);
+			this.list.Name = "list";
+			this.list.Size = new System.Drawing.Size(236, 508);
+			this.list.TabIndex = 0;
+			// 
+			// Window
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(240)))), ((int)(((byte)(240)))));
+			this.ClientSize = new System.Drawing.Size(847, 508);
+			this.Controls.Add(this.splitContainer1);
+			this.Menu = this.mainMenu1;
+			this.Name = "Window";
+			this.Text = "Form1";
+			this.splitContainer1.Panel1.ResumeLayout(false);
+			this.splitContainer1.Panel2.ResumeLayout(false);
+			this.splitContainer1.Panel2.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+			this.splitContainer1.ResumeLayout(false);
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.SplitContainer splitContainer1;
+		private System.Windows.Forms.MainMenu mainMenu1;
+		private System.Windows.Forms.MenuItem menuItem1;
+		private System.Windows.Forms.MenuItem menuItem2;
+		private System.Windows.Forms.MenuItem menuItem3;
+		private System.Windows.Forms.MenuItem menuItem4;
+		private System.Windows.Forms.MenuItem menuItem5;
+		private System.Windows.Forms.MenuItem menuItem6;
+		private System.Windows.Forms.MenuItem menuItem7;
+		private System.Windows.Forms.MenuItem menuItem8;
+		private System.Windows.Forms.MenuItem menuItem9;
+		private System.Windows.Forms.MenuItem menuItem10;
+		private System.Windows.Forms.MenuItem menuItem11;
+		private System.Windows.Forms.MenuItem menuItem12;
+		private System.Windows.Forms.MenuItem menuItem13;
+		private System.Windows.Forms.MenuItem menuItem14;
+		private System.Windows.Forms.MenuItem menuItem15;
+		private System.Windows.Forms.MenuItem menuItem16;
+		private System.Windows.Forms.MenuItem menuItem17;
+		private System.Windows.Forms.MenuItem menuItem18;
+		private System.Windows.Forms.MenuItem menuItem19;
+		private System.Windows.Forms.MenuItem menuItem20;
+		private System.Windows.Forms.MenuItem menuItem21;
+		private System.Windows.Forms.MenuItem menuItem22;
+		private System.Windows.Forms.MenuItem menuItem23;
+		private System.Windows.Forms.MenuItem menuItem24;
+		private System.Windows.Forms.MenuItem menuItem25;
+		private System.Windows.Forms.MenuItem menuItem26;
+		private System.Windows.Forms.MenuItem menuItem27;
+		private System.Windows.Forms.MenuItem menuItem28;
+		private System.Windows.Forms.MenuItem menuItem29;
+		private System.Windows.Forms.MenuItem menuItem30;
+		private System.Windows.Forms.TextBox editor;
+		private System.Windows.Forms.TreeView list;
+	}
+}
+
